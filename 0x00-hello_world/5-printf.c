33765b3d3d2b146ce9dc114a1c84c9fd447f0885
Salim Kachemela <sakachemela@gmail.com>
2022-04-07 16:58:07 -0700

    A C program that prints exactly with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * Program: 4-puts.c
+ * The Program prints some text to the standard output.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2019
+ */
+#include <stdio.h>
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
