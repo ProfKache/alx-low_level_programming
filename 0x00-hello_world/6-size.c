7b52d1de744591f5e4229f122f7d03e24ba1f8f3
Salim Kachemela <sakachemela@gmail.com>
2022-04-07 17:23:50 -0700

    A C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %c byte(s)", sizeof(char));
-printf("Size of an int: %i byte(s)", sizeof(int));
-printf("Size of a long int: %i byte(s)", sizeof(long int));
-printf("Size of a long long int: %d byte(s)", sizeof(long long int));
-printf("Size of a float: %i byte(s)", sizeof(float));
+printf("Size of a char: %zu byte(s)", sizeof(char));
+printf("Size of an int: %zu byte(s)", sizeof(int));
+printf("Size of a long int: %zu byte(s)", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
+printf("Size of a float: %zu byte(s)", sizeof(float));
 
 return (0);
 }
