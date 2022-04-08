722d8669bac6c68a8fc83cc56a95525821905f6e
Salim Kachemela <sakachemela@gmail.com>
2022-04-07 18:31:10 -0700

    A C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ * Program: 4-puts.c
+ * The Program prints text to the output without using printf or puts.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2019
+ */
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
