1f92458b06a509eea185b14cb296bc8965914467
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 19:01:35 -0700

    Task 3: A program that prints the alphabet in lowercase, and then in uppercase, followed by a new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+/*
+ * Program: 2-print_alphabet.c
+ * The Program prints the alphabets in lowercase, and then in uppercase, followed by a new line.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright(c) 2022
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
