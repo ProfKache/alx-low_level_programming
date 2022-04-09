1b3cf60f8829aaa64bcb849da6bc913b3a212c33
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 19:21:20 -0700

    Task 6: A program that prints all single digit numbers of base 10 starting from 0, followed by a new line

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * Program: 2-print_alphabet.c
+ * The Program prints all single digit numbers of base 10 starting from 0.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright(c) 2022
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+	char ch = '1';
+
+	for (ch = '1'; ch <= '9'; ch++)
+		putchar(ch);
+	putchar('\n');
+
+	return (0);
+}
