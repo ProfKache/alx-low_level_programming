960dc360eb9aa04b921fd7d3081375f55dd57dbf
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 17:07:26 -0700

    Task 0: A C program to print whether the number stored in the variable n is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+/*
+ * Program: 0-positive_or_negative.c
+ * The Program prints whether the number stored in the variable n is positive
+ * or negative.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright(c) 2022
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n > 0 )
+		puts("is positive");
+	else if (n == 0)
+		puts("is zero");
+	else
+		puts("is negative");
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+My First Low Level Repository
