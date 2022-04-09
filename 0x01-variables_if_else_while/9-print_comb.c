e25bfc30e6bcd0133587ea65ce0f2880ad1d1cf3
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 19:46:32 -0700

    Task 9: A program that prints all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+/*
+ * Program: 2-print_alphabet.c
+ * The Program prints all single rints all possible combinations
+ * of single-digit numbers
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright(c) 2022
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+	int ch = '1';
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+		putchar('\,');
+	putchar('\b');
+
+	return (0);
+}
