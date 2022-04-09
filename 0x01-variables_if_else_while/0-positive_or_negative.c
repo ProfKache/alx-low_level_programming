9a56530b306b850eaecbcd25aa0a0232957bb769
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 17:15:00 -0700

    Task 0: Fix Correct Output

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,11 +20,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d\n is positive", n);
+		printf("%d is positive\n", n);
 	else if (n == 0)
-		printf("%d\n is zero", n);
+		printf("%d is zero\n", n);
 	else
-		printf("%d\n is negative", n);
+		printf("%d is negative\n", n);
 
 	return (0);
 }
