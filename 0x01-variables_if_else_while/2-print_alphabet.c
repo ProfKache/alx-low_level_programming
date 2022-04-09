f9241295680b643a7f3c77e1763456e0822dca33
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 18:50:36 -0700

    Task 2: A program that prints the alphabet in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * Program: 2-print_alphabet.c
+ * The Program prints the alphabets in lowercase, followed by a new line.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright(c) 2022
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+	char ch = 'a';
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch)
+	putchar();
+
+	return (0);
+}
