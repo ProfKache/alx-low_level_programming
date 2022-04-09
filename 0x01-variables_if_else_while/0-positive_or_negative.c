92e19b29b7c0cd0c23071326725cf8e306ad8ae1
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 17:12:37 -0700

    Task 0: Fix Coding Style and Correct Output

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,13 +19,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if (n > 0 )
-		puts("is positive");
+	if (n > 0)
+		printf("%d\n is positive", n);
 	else if (n == 0)
-		puts("is zero");
+		printf("%d\n is zero", n);
 	else
-		puts("is negative");
+		printf("%d\n is negative", n);
 
 	return (0);
 }
