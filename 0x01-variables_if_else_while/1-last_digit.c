5f24274ec12a16636ee6a942c7532e55771b5795
Salim Kachemela <sakachemela@gmail.com>
2022-04-08 18:32:04 -0700

    Task 1: A C program to print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,13 +19,14 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	lastDigit = n % 10;
 	printf("Last digit of %d is ", n);
 	if (n > 5)
-		printf("%d and is greater than 5\n", n % 10);
+		printf("%d and is greater than 5\n", lastDigit);
 	else if (n == 0)
-		printf("%d and is 0\n", n % 10);
+		printf("%d and is 0\n", lastDigit);
 	else if (n < 6 && n != 0)
-		printf("%d and is less than 6 and not 0\n", n % 10);
+		printf("%d and is less than 6 and not 0\n", lastDigit);
 
 	return (0);
 }
