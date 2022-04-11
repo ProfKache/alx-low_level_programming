0ad277139ed491a2a97f7b1bac4cf9a129df4519
Salim Kachemela <sakachemela@gmail.com>
2022-04-10 19:34:43 -0700

    Task 10: Fix display of unwanted numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -20,22 +20,23 @@ int main(void)
 
         for (i = 48; i < 57; i++)
         {
-          for (j = 49; j < 58; j++)
-          {
-              if (j > i)
-              {
-                putchar(i);
-                putchar(j);
 
-                if (i != 56 || j != 57)
-                {
+		  for (j = 49; j < 58; j++)
+		  {
+			  if (j > i)
+			  {
+				  putchar(i);
+				  putchar(j);
 
-                    putchar(',');
-                    putchar(' ');
-                }
-              }
-          }
-        }
+				  if (i != 56 || j != 57)
+				  {
+					  putchar(',');
+					  putchar(',');
+				  }
+			  }
+		  }
+	}
+
         putchar('\n');
 
         return (0);
