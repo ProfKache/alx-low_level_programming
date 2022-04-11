19b3ca5a3158c222a8f5ff1428982f762488bf34
Salim Kachemela <sakachemela@gmail.com>
2022-04-11 12:15:28 -0700

    A program that prints all possible different combinations of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,42 @@
+/*
+ * Program: 2-print_alphabet.c
+ * The Program prints all single possible different combinations of two digits.
+ * of single-digit numbers
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright(c) 2022
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar((i % 10) + '0');
+				putchar((j % 10) + '0');
+				putchar((k % 10) + '0');
+				
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
