444791a5245ddceff33b62e6b9bf59495fa4d415
Salim Kachemela <sakachemela@gmail.com>
2022-04-12 00:21:18 -0700

    Task 0: A program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+ * Program: 0-putchar.c
+ * The Program prints _putchar, followed by a new line.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2019
+ */
+#include <unistd.h>
+/**
+ * main - The Entry point of the program.
+ * Return: 0 if the program runs without errors.
+ */
+int main(void)
+{
+	char text[] = "_putchar";
+	printf("%s\n", text);
+	return (0);
+}
