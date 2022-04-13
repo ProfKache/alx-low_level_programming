edde2f71c2ac69705d7b4df99d7f029e9498f099
Salim Kachemela <sakachemela@gmail.com>
2022-04-12 19:04:33 -0700

    Task 3: A function that checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+/*
+ * Program: 1-alphabet.c
+ * A function that checks for lowercase characters.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters.
+ */
+int _islower(int c)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == c)
+			return 1;
+		else
+			return 0;
+		alphabet++;
+	}
+}
