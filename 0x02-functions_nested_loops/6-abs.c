4d437ccdadf32f6db5c9ede4acfd126bc5d5c3fd
Salim Kachemela <sakachemela@gmail.com>
2022-04-13 08:50:58 +0300

    Task 6: A function that returns the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+/*
+ * Program: 5-sign.c
+ * A function that computes the absolute value of an integer
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @int: The parameter to be evaluated with.
+ *
+ * Return: the absolute value of an integer;
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-1 * n);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (n);
+	}
+}
