4608e0aed3c084fe3b0eb537328af91510bcf5f1
Salim Kachemela <sakachemela@gmail.com>
2022-04-13 11:59:21 +0300

    Fix Output on negative numbers

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -17,6 +17,14 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	_putchar('0' + last_digit);
-	return (last_digit);
+	if (n > 0)
+	{
+		_putchar('0' + last_digit);
+		return (last_digit);
+	}
+	else
+	{
+		_putchar('0' + (-last_digit));
+		return (-last_digit);
+	}
 }
