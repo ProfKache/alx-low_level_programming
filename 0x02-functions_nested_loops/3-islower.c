87f3b4bc3c56c966b8315856dcaf985bbb9a56bd
Salim Kachemela <sakachemela@gmail.com>
2022-04-12 19:29:34 -0700

    Fix some errors related to Betty Style on function parameter and return type

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,16 +8,16 @@
 
 /**
  * _islower - checks for lowercase characters.
+ * 
+ * @c: The parameter to be evaluated with.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
 
 	if (c >= 97 && c <= 122)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
