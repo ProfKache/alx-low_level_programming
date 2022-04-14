1968e8fafddd8cc2ae32f7bed6246b24b4a04b22
Salim Kachemela <sakachemela@gmail.com>
2022-04-14 05:44:03 +0300

    Fix Correct output

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,22 +18,28 @@ void times_table(void)
 	int rows;
 	int columns;
 	int result = 0;
+	int first_digit;
+	int last_digit;
 
 	for (rows = 0; rows < 10; rows++)
 	{
 		for (columns = 0; columns < 10; columns++)
 		{
 			result = rows * columns;
+			first_digit = result / 10;
+			last_digit = result % 10;
 
 			if (result >= 0 && result <= 9)
 			{
-				_putchar(result + '0');
+				_putchar(first_digit + '0');
+				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 			}
-			else if (result >= 10 && result <=99)
+			if (result >= 10 && result <=99)
 			{
-				_putchar(result + '0');
+				_putchar(first_digit + '0');
+				_putchar(last_digit + '0');
 				_putchar(',');
 				_putchar(' ');
 			}
