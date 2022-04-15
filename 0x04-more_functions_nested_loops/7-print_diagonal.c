fe5f264d60ea5ed38be4c7688beff79f555ae89a
Salim Kachemela <sakachemela@gmail.com>
2022-04-15 16:31:19 +0300

    Task 7: A unction that draws a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+/*
+ * Program: 7-print_diagonal.c
+ * A function that draws a diagonal line on the terminal.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal.
+ *
+ * @n: The parameter to print the number of times the character \ should
+ * be printed
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= i; j++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
