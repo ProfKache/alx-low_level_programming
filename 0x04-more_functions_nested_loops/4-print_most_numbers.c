e947ed5cdb02916043ffa5e7c2f775264b690cf1
Salim Kachemela <sakachemela@gmail.com>
2022-04-15 05:45:36 +0300

    Fix betty errors and output errors

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -14,9 +14,8 @@ void print_most_numbers(void)
 int i;
 for (i = 48; i < 58; i++)
 {
-	if (i == 50 && i == 52)
-		continue;
-	_putchar(i);
+	if (i != 50 && i != 52)
+		_putchar(i);
 }
 _putchar('\n');
 }
