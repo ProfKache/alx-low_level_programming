5e9871c47aab2f2b95bece36d869ae81664c93a5
Salim Kachemela <sakachemela@gmail.com>
2022-04-16 02:47:58 +0300

    Task 8: A function that prints a square, followed by a new line

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+/*
+ * Program: 8-print_square.c
+ * A function that prints square, followed by a new line.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+
+/**
+ * print_square - draws a diagonal line on the terminal.
+ * @size: The parameter to print the size of the square.
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
