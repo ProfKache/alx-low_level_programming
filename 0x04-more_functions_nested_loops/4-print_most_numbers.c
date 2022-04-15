690b1bd53b9e049ddb492c48044c9a7d321ef50f
Salim Kachemela <sakachemela@gmail.com>
2022-04-15 05:52:37 +0300

    Fix some issues

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,13 +12,15 @@
 void print_most_numbers(void)
 {
 int i;
-for (i = 48; i < 58; i++)
+i = 0;
+while (i < 10)
 {
-if (i != 50 && i != 52)
+if (i != 2 && i != 4)
 {
-_putchar(i);
+_putchar (i + '0');
 }
+i++;
 }
 
-_putchar('\n');
+_putchar ('\n');
 }
