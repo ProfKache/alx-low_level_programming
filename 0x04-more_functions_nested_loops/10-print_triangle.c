bd1d8bc410e70ef909b12252e078467f6b3afeed
Salim Kachemela <sakachemela@gmail.com>
2022-04-16 04:16:18 +0300

    Fix output errors

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,6 +15,7 @@ void print_triangle(int size)
 {
 	int i;
 	int j;
+	int k;
 
 	if (size <= 0)
 	{
@@ -24,6 +25,9 @@ void print_triangle(int size)
 	{
 		for (i = 1; i <= size; i++)
 		{
+            for (k = size; k >= i; k--)
+                _putchar(' ');
+
 			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
