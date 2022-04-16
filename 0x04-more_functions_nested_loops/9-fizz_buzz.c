a4002a76d77bb57cfac9cf9ab47be1c4893a9e5f
Salim Kachemela <sakachemela@gmail.com>
2022-04-16 03:21:23 +0300

    A program that prints the numbers from 1 to 100, followed by FizzBuzz Logic

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+/*
+ * Program: 9-fizz_buzz.c
+ * This is a FizzBuzz Program.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	putchar('\n');
+}
