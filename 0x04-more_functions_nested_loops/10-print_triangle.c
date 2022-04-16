ce02fd673fcd617c2f8c81e219578d08df82020c
Salim Kachemela <sakachemela@gmail.com>
2022-04-16 05:32:50 +0300

    Fix betty style error

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,27 +13,27 @@
  */
 void print_triangle(int size)
 {
-	int i;
-	int j;
-	int k;
+int i;
+int j;
+int k;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 1; i <= size; i++)
-		{
-			for(j = i; j < size; j++)
-			{
-				_putchar(' ');
-			}
-			for (k = 1; k <= i; k++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for(j = i; j < size; j++)
+{
+_putchar(' ');
+}
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
