bb43b50ac9afa49331f4be06aa631027b7cd8e2f
Salim Kachemela <sakachemela@gmail.com>
2022-04-17 14:34:01 +0300

    Task 12: A function that prints an integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,31 @@
+/*
+ * Program: 101-print_number.c
+ * A function that draws a straight line in the terminal.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: The parameter to be printed as a number.
+ * be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	int i;
+
+	if (n > 0)
+		i = n;
+	else
+	{
+		_putchar('-');
+		i = -n;
+	}
+	if (i / 10 != 0)
+		print_number(i / 10);
+	_putchar((i % 10) + '0');
+}
