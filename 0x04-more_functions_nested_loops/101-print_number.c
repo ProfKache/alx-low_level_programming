35bafc011a487cab36f03cc8bcc206ab27a5189e
Salim Kachemela <sakachemela@gmail.com>
2022-04-17 14:51:29 +0300

    Fix some errors

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,28 +8,25 @@
 
 /**
  * print_number - prints an integer.
- *
  * @n: The parameter to be printed as a number.
- * be printed
- *
  * Return: void
  */
 void print_number(int n)
 {
-	unsigned int i;
+unsigned int i;
 
-	if (n > 0)
-	{
-		i = n;
-	}
-	else
-	{
-		_putchar('-');
-		i = -n;
-	}
-	if (i / 10 != 0)
-	{
-		print_number(i / 10);
-	}
-	_putchar((i % 10) + '0');
+if (n > 0)
+{
+i = n;
+}
+else
+{
+_putchar('-');
+i = -n;
+}
+if (i / 10 != 0)
+{
+print_number(i / 10);
+}
+_putchar((i % 10) + '0');
 }
