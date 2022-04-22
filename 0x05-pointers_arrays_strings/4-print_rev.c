0272c6bf3a83a457c715b643950ad69236230d21
Salim Kachemela <sakachemela@gmail.com>
2022-04-22 07:01:58 +0300

    Add Task 4: A function that reverses a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+/*
+ * Program: 4-print_rev.c
+ * A function that prints a string in reverse.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - function that prints a string in reverse.
+ *
+ * @s: the parameter to be printed to std output in reverse.
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int l = strlen(s);
+
+	for (i = l ; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
