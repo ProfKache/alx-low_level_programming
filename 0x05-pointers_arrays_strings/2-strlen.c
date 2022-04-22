0dac3de3b5fedd2e914a89aae61b56e80e24bea5
Salim Kachemela <sakachemela@gmail.com>
2022-04-22 05:43:15 +0300

    Add Task 2: function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/*
+ * Program: 2-strlen.c
+ * A function that returns the length of a string.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function for returning the string length
+ *
+ * @s: The string parameter to be calculated
+ *
+ * Return: number
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
