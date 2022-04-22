7a271ad403db7e79092ef17661af99c23d532845
Salim Kachemela <sakachemela@gmail.com>
2022-04-22 05:18:18 +0300

    Add: Task 0 - function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+/*
+ * Program: 0-reset_to_98.c
+ * A function that takes a pointer to an int as parameter and updates
+ * the value it points to to 98.
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+/**
+ * reset_to_98 - resets and updates and integer
+ *
+ * @n: The parameter to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
