a53c66c7d0e685282c714fea6fd8b47c16246cb4
Salim Kachemela <sakachemela@gmail.com>
2022-04-26 11:35:37 +0300

    Fix output issues

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,9 +17,9 @@ void puts_half(char *str)
 {
 	int s = 0;
 	int l = strlen(str);
-	int half_string = l / 2;
+	int half_string = (l - 1) / 2;
 
-	for (s = half_string; s <= l ; s++)
+	for (s = half_string; s < l ; s++)
 	{
 		_putchar(str[s]);
 	}
