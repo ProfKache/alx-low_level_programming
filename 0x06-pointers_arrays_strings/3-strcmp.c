f5fbb127ade263504e10ae3ce269309c643adeb4
Salim Kachemela <sakachemela@gmail.com>
2022-04-27 22:38:03 +0300

    Add Task 2: A functio that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+/*
+ * Program: 3-strcmp.c
+ * A function that compares two strings
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - function that compares two strings.
+ * @s1: the pointer parameter for first string
+ * @s2: the pointer parameter for second string
+ * Return: a pointer string.
+ */
+int _strcmp(char *s2, char *s1)
+{
+	return (strcmp(s1, s1));
+}
