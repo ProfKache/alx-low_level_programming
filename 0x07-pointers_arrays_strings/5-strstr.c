64da3e30d2fbfa0af44425950b771771cc732a2a
Salim Kachemela <sakachemela@gmail.com>
2022-04-28 01:47:35 +0300

    Add Task 5: a function returning a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+/*
+ * Program: 5-strstr.c
+ * A function that locates a substring
+ * Author: Salim Kachemela <sakachemela@gmail.com>
+ * Copyright @2022
+ */
+#include "main.h"
+/**
+ * strstr - function that locates a substring
+ * @haystack: the pointer string.
+ * @needle: the character occured.
+ * Return: a substring.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int a = 0, b = 0;
+
+while (haystack[a])
+{
+while (needle[b])
+{
+if (haystack[a + b] != needle[b])
+{
+break;
+}
+
+b++;
+}
+
+if (needle[b] == '\0')
+{
+return (haystack + a);
+}
+
+a++;
+}
+
+return ('\0');
+}
