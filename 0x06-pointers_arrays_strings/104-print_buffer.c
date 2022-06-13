5fbd772d2d724118fa86d6fa68d84af7e3b9058b
Salim Kachemela <sakachemela@gmail.com>
2022-06-13 19:19:47 +0300

    Fix output issues

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -4,46 +4,45 @@
  * Author: Salim Kachemela <sakachemela@gmail.com>
  * Copyright @2022
  */
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_buffer - A function that adds two numbers.
- * @b: the address of the buffer.
- * @size: the size to be printed
- * Return: void
+ *print_buffer - Print a buffer 10 bytes per line.
+ *@b: Buffer address.
+ *@size: Number of characters to be printed.
  */
 void print_buffer(char *b, int size)
 {
-	int i, j, k;
+int j, k, l;
 
-	if (size <= 0)
-		printf("\n");
-	else
-	{
-		for (i = 0; j < size; i += 10)
-		{
-			printf("%.8x:", i);
-			for (j = i; j < i + 10; j++)
-			{
-				if (j % 2 == 0)
-					printf(" ");
-				if (j < size)
-					printf("%.2x", *(b + j));
-				else
-					printf("  ");
-			}
-			printf(" ");
-			for (k = i; k < i + 10; k++)
-			{
-				if (k >= size)
-					break;
-				if (*(b + k) < 32 || *(b + k) > 126)
-					printf("%c", '.');
-				else
-					printf("%c", *(b + k));
-			}
-			printf("\n");
-		}
-	}
+if (size <= 0)
+printf("\n");
+else
+{
+for (j = 0; j < size; j += 10)
+{
+printf("%.8x:", j);
+for (k = j; k < j + 10; k++)
+{
+if (k % 2 == 0)
+printf(" ");
+if (k < size)
+printf("%.2x", *(b + k));
+else
+printf("  ");
+}
+printf(" ");
+for (l = j; l < j + 10; l++)
+{
+if (l >= size)
+break;
+if (*(b + l) < 32 || *(b + l) > 126)
+printf("%c", '.');
+else
+printf("%c", *(b + l));
+}
+printf("\n");
+}
+}
 }
