b2bd23db0380ca1005470c52b8e52d3689e07242
Salim Kachemela <sakachemela@gmail.com>
2022-06-13 18:19:55 +0300

    Fix output issues

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -5,6 +5,31 @@
  * Copyright @2022
  */
 #include "main.h"
+
+/**
+ * string_reverse - A function for reversint the array string
+ * @n: A character pointer parameter.
+ * Return: 0
+ */
+void string_reverse(char *n)
+{
+	int i = 0, k = 0;
+	char tmp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (k = 0; k < i; k++, i--)
+	{
+		tmp = *(n + k);
+		*(n + k) = *(n + i);
+		*(n + i) = tmp;
+	}
+}
+
 /**
  * infinite_add - A function that adds two numbers.
  * @n1: the first number.
@@ -15,7 +40,44 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	*r = *n1 + *n2;
+	int overflow = 0;
+	int i = 0, k = 0, digits = 0;
+	int value1 = 0, value2 = 0, tmp_total = 0;
 
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + k) != '\0')
+		k++;
+	i--;
+	k--;
+	if (k >= size_r || i >= size_r)
+		return (0);
+	while (k >= 0 || k >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			value1 = 0;
+		else
+			value1 = *(n1 + i) - '0';
+		if (k < 0)
+			value2 = 0;
+		else
+			value2 = *(n2 + k) - '0';
+		tmp_total = value1 + value2 + overflow;
+		if (tmp_total >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (tmp_total % 10) + '0';
+		digits++;
+		k--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	string_reverse(r);
 	return (r);
 }
