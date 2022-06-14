0255f12a4cd4bf282bba6f214c9f99ec43a1835d
Salim Kachemela <sakachemela@gmail.com>
2022-06-14 13:27:57 +0300

    Fix output errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,48 +1,51 @@
-/*
- * A Program that prints the minimum number of coins to make change for
- * an amount of money.
- * Author: Salim J. Kachemela <sakachemela@gmail.com>
- * Copyright (c)2022
- */
-
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
- * main - An entry point
- * @argc: argument counter
- * @argv: argument vector
- * Return: Always return success
+ * main - function
+ *@argc: length of argv
+ *@argv: number of argument
+ *Return: Always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int position = 0, total = 0, change = 0, aux = 0;
-	int coins[] = {25, 10, 5, 1};
+/*Declaring variables*/
+int position, total, change, aux;
+int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
 
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	total = atoi(argv[1]);
+position = total = change = aux = 0;
 
-	if (total <= 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 
-	while (coins[position] != '\0')
-	{
-		if (total >= coins[position])
-		{
-			aux = (total / coins[position]);
-			change += aux;
-			total -= coins[position] * aux;
-		}
-		position++;
-	}
-	printf("%d\n", change);
-	return (0);
+total = atoi(argv[1]); /*Covert str to int*/
+
+if (total <= 0)
+{
+printf("0\n");
+return (0);
+}
+
+/*Declaring While*/
+
+while (coins[position] != '\0')
+
+{
+if (total >= coins[position])
+{
+aux = (total / coins[position]);
+change += aux;
+total -= coins[position] * aux;
+}
+
+position++;
+
+}
+
+printf("%d\n", change);
+return (0);
 }
