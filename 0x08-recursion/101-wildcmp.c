3e552289de92a06a7aacf906063ab14bc6b08629
Salim Kachemela <sakachemela@gmail.com>
2022-06-14 04:05:12 +0300

    Add functin definition for task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * wildcmp - A function that compares two strings.
+ * @s1: A first string.
+ * @s2: A second string.
+ * Return: 1 if string a identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
