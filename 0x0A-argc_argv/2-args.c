d688b9bcc9e3e895d07d533c0058cac56c2af1a7
Salim Kachemela <sakachemela@gmail.com>
2022-06-14 11:41:24 +0300

    Add solution for task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+/*
+ * A Program that prints the number of arguments passed into it
+ * Author: Salim J. Kachemela <sakachemela@gmail.com>
+ * Copyright (c)2022
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - An entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always return success
+ */
+int main(int argc, char **argv)
+{
+	for (int i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
