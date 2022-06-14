e215c1b08f9128ea6e682bfa94a86b5df06dba13
Salim Kachemela <sakachemela@gmail.com>
2022-06-14 13:24:29 +0300

    Fix output errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 	{
 		if (total >= coins[position])
 		{
-			aux = total / coins[position];
+			aux = (total / coins[position]);
 			change += aux;
 			total -= coins[position] * aux;
 		}
