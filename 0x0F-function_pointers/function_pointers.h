1f72ffcfd5f7aa628a9df1f0c0d6ff34fdae2a07
Salim Kachemela <sakachemela@gmail.com>
2022-06-15 13:19:25 +0300

    Add some fixing the header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdlib.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+#endif
