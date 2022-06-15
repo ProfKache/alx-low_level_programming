51d69f22e6e0799ed36f55c37e6970b0bb833c14
Salim Kachemela <sakachemela@gmail.com>
2022-06-15 13:02:48 +0300

    Fix some output errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,13 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - Function to call a function
- *
  * @name: Parameter 1
  * @f: parameter function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
-	{
-		if (name)
-		{
-			(*f)(name);
-		}
-	}
+	if (!name || !f)
+		return;
+	f(name);
 }
