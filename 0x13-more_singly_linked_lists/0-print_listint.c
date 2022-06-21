bc8bfd636fe267d9f59e81311d01d98a0c5c22de
Salim Kachemela <sakachemela@gmail.com>
2022-06-21 13:10:44 +0300

    Fix output errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,26 @@
 #include "lists.h"
-
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * print_listint - function that prints all the elements of
+ * a listint_t list.
  *
- * Return: number of nodes
+ * @h: Parameter Pointer to the head of the linked list
+ *
+ * Return: the number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-size_t num = 0;
+	size_t n = 0;
 
-while (h)
-{
-printf("%d\n", h->n);
-num++;
-h = h->next;
+	if (!h)
+	{
+		return (0);
+	}
+	while (h)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		n++;
+	}
+	return (n);
 }
 
-return (num);
-}
