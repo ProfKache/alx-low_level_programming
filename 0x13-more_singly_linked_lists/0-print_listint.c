0b3b671b8f3b7400f971d72c690315962db4137a
Salim Kachemela <sakachemela@gmail.com>
2022-06-21 13:03:16 +0300

    Fix output errors

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t n = 0;
 
-	if (!h)
+	if (h == NULL)
 	{
 		return (0);
 	}
-	while (h)
+	while (h != NULL)
 	{
 		printf("%i\n", h->n);
 		h = h->next;
