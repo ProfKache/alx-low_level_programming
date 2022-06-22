e49e148283739f93da72131d8bd0fdfe44069bb4
Salim Kachemela <sakachemela@gmail.com>
2022-06-22 09:46:30 +0300

    Fix some errors with ouput

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_listint - function that prints all the elements of
  * a listint_t list.
@@ -9,14 +10,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t n = 0;
+	size_t num = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
-		n++;
+		num++;
 		h = h->next;
 	}
-	return (n);
+	return (num);
 }
 
